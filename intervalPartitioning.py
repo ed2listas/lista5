ba5fee5911b3bd402a465359d1fc775203149739
Joberth Rogers <joberth.rogers18@gmail.com>
2018-10-12 13:45:55 -0300

    creating objects from file and sort them

--- intervalPartitioning.py
+++ intervalPartitioning.py
@@ -1,6 +1,8 @@
 import csv
 from funcoes_auxiliares import *
+import random
 
+classrooms_file = []
 classrooms = []
 start_activity = 8
 finish_activity = 20
@@ -10,21 +12,38 @@ class Classroom:
     finish = 0
     name = ""
 
-    def __init__ (self, start, finish, name):
+    def __init__ (self,name,start, finish):
         self.start = start
         self.finish = finish
         self.name = name
 
     
 with open ("companies.csv", "r") as f:
-    data = csv.DictReader(f, delimiter=",")
-    #next(dados) # Descarta o cabeçalho
+    data = csv.reader(f)
+    next(data)
 
     for line in data:
-        classrooms.append(list(line.values()))
+        classrooms_file.append(Classroom(line[0], int(line[1]), int(line[2])))
 
-    for i in classrooms:
-        print(i)
+    for i in classrooms_file:
+        print(i.name)
+
+
+classrooms = random.sample(classrooms_file, 15)
+
+for i in classrooms:
+        print(i.start)
+
+print("\n")
+
+classrooms.sort(key=lambda x: x.start)
+
+
+for i in classrooms:
+        print(i.start)
+
+
+
 
 
 
